5e610bfdb19d7d3ac48d3a1d584b012312a56ff5
evewolfs <53898640+evewolfs@users.noreply.github.com>
2024-05-05 18:02:02 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import ReactDOM from "react-dom/client";
 import "./styles.css";
 import { Canvas } from "@react-three/fiber";
+import {Perf} from "r3f-perf"
 import Scene from "./Scene";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     <Canvas>
       <Scene />
+      <Perf position="top-left" />
     </Canvas>
   </>
 );
